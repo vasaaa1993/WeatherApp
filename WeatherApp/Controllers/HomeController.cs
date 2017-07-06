4fd555424c591f815cd985b8988d593cf4430077
Vasyl Barna <vasaaa1993@gmail.com>
2017-07-06 12:15:03 +0300

    added cusstom icons

--- WeatherApp/Controllers/HomeController.cs
+++ WeatherApp/Controllers/HomeController.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WeatherApp.Services;
 
 namespace WeatherApp.Controllers
 {
 	public class HomeController : Controller
 	{
+		private static WeatherService _weatherService;
+		public HomeController()
+		{
+			if (_weatherService == null)
+				_weatherService = new WeatherService();
+		}
+
 		public ActionResult Index()
 		{
 			return View();
