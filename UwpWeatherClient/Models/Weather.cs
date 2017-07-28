dc491b64d3c66427fbfdc584b5e6c91493a12837
Vasyl Barna <vasaaa1993@gmail.com>
2017-07-28 16:18:55 +0300

    added new project

---
+++ UwpWeatherClient/Models/Weather.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace WeatherApp.Models
+{
+	public class Weather
+	{
+		public Weather()
+		{
+			WeatherList = new List<WeatherListItem>();
+		}
+
+		public string CityName { get; set; }
+		public string CountryCodeOfTheCity { get; set; }
+		public List<WeatherListItem> WeatherList { get; set; }
+	}
+}
---
+++ UwpWeatherClient/Models/WeatherListItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace WeatherApp.Models
+{
+	public class WeatherListItem
+	{
+		public DateTime Time { get; set; }
+
+		public double MinTemp { get; set; }
+		public double MaxTemp { get; set; }
+		public double DayTemp { get; set; }
+
+		public string Description { get; set; }
+		public double Humidity { get; set; }
+		public double Pressure { get; set; }
+		public int Clouds { get; set; }
+		public double WindSpeed { get; set; }
+
+		public string Icon { get; set; }
+	}
+}
