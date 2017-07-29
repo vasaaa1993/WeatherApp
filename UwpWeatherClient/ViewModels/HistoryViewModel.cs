26f822f758f02c40110c0f313f7bffa729b15e35
Vasyl Barna <vasaaa1993@gmail.com>
2017-07-29 19:10:53 +0300

    finished works with HistoryView

--- UwpWeatherClient/ViewModels/HistoryViewModel.cs
+++ UwpWeatherClient/ViewModels/HistoryViewModel.cs
@@ -1,10 +1,12 @@
-﻿using GalaSoft.MvvmLight.Views;
+﻿using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.Views;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using UwpWeatherClient.Models;
 using UwpWeatherClient.Services;
 
@@ -15,25 +17,43 @@ namespace UwpWeatherClient.ViewModels
 		private INavigationService _navigationService;
 		private IHistoryService _historyService;
 
-		public List<History> History { get; private set; }
+		public ObservableCollection<History> History { get; private set; }
+
+		public ICommand ReloadHistoryCommand { get; set; }
+		public ICommand ClearHistoryCommand { get; set; }
+
 		public HistoryViewModel(INavigationService navigationService,
 			IHistoryService historyService)
 		{
 			_navigationService = navigationService;
 			_historyService = historyService;
 
-			History = new List<History>();
-			GetHistory();
+			Init();
 		}
-
-		private async void GetHistory()
+		private void Init()
+		{
+			History = new ObservableCollection<History>();
+			ReloadHistoryCommand =  new RelayCommand(ReloadHistory);
+			ClearHistoryCommand = new RelayCommand(ClearHistory);
+			ReloadHistory();
+		}
+		private async void ReloadHistory()
 		{
 			var h = await _historyService.GetHistory();
 			if (h != null)
 			{
-				History.AddRange(h);
+				foreach(var item in h)
+					History. Add(item);
+
 				RaisePropertyChanged(() => History);
 			}
+		}
+
+		private async void ClearHistory()
+		{
+			var rez = await _historyService.ClearHistory();
+			if(rez)
+				RaisePropertyChanged(() => History);
 		}
 	}
 }
