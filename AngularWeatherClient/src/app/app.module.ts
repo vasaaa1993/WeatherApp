9a9d79e3db68adc27d2f9a557b0ae0b54930764c
Vasyl Barna <vasaaa1993@gmail.com>
2017-07-30 16:01:54 +0300

    added AngularClient

---
+++ AngularWeatherClient/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HistoryComponent } from './history/history.component';
+import { CitiesComponent } from './cities/cities.component';
+import { WeatherComponent } from './weather/weather.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HistoryComponent,
+    CitiesComponent,
+    WeatherComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
