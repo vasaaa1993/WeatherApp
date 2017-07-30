7c3c45b1d58898e05b33b5d219925663c9f3f2d4
Vasyl Barna <vasaaa1993@gmail.com>
2017-07-30 08:57:44 +0300

    improved designg

---
+++ UwpWeatherClient/Converters/DateLongFormatConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace UwpWeatherClient.Converters
+{
+	class DateLongFormatConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, string language)
+		{
+			if (value == null)
+				return null;
+			DateTime dt = (DateTime)value;
+			return dt.ToString("dd MMM HH:mm:ss");
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, string language)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
--- UwpWeatherClient/Converters/DateShortFormatConverter.cs
+++ UwpWeatherClient/Converters/DateShortFormatConverter.cs
@@ -3,7 +3,7 @@ using Windows.UI.Xaml.Data;
 
 namespace UwpWeatherClient.Converters
 {
-	class DateFormatConverter:IValueConverter
+	class DateShortFormatConverter:IValueConverter
 	{
 		public object Convert(object value, Type targetType, object parameter, string language)
 		{
